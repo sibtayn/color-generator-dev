f23d27a0c4436c1fb9294be7b2d059c05bf4aaa7
sibtayn <161344158+sibtayn@users.noreply.github.com>
2024-05-08 22:33:24 +0530

    Add files via upload

---
+++ apna-js.js
@@ -0,0 +1,23 @@
+
+let btn = document.querySelector("button");
+btn.addEventListener("click" , function(){
+    let h2 = document.querySelector("h2");
+    let randomcolor = getrandomcolor();
+    h2.innerText = randomcolor;
+
+    let colo = document.querySelector(".color");
+    colo.style.backgroundColor = randomcolor;
+    
+});
+function getrandomcolor(){
+    let red= Math.floor(Math.random()*255);
+    let green= Math.floor(Math.random()*255);
+    let blue= Math.floor(Math.random()*255);
+
+    let color= `rgb(${red}, ${green}, ${blue})`;
+    return color;
+
+}
+
+
+
